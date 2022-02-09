8dff4b9b9df9c4660486f517d898751ad08c1cae
LazyLad <lazylad23@gmail.com>
2022-02-09 19:29:05 +0530

    logout button added on home screen

--- lib/screens/home/home.dart
+++ lib/screens/home/home.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:sign_in_system/services/auth_method_file.dart';
 
 class Home extends StatefulWidget {
   const Home({Key? key}) : super(key: key);
@@ -8,6 +9,7 @@ class Home extends StatefulWidget {
 }
 
 class _HomeState extends State<Home> {
+  final AuthService _auth = AuthService();
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -15,6 +17,23 @@ class _HomeState extends State<Home> {
       appBar: AppBar(
         centerTitle: true,
         title: const Text("Home Page"),
+        actions: [
+          TextButton.icon(
+            onPressed: () async {
+              await _auth.signOut();
+            },
+            icon: const Icon(
+              Icons.person,
+              color: Colors.black,
+            ),
+            label: const Text(
+              "Logout",
+              style: TextStyle(
+                color: Colors.black,
+              ),
+            ),
+          )
+        ],
       ),
     );
   }
